6a41032ae214135d99726d862d8b22df806459b4
wisnienka <katarzyna.sowa@silvair.com>
2017-08-28 09:55:10 +0200

    final commit

---
+++ README.md
@@ -0,0 +1,5 @@
+It is basically command line scanner application. 
+
+Search engine is like file loader, which scan only .txt files. 
+
+It has counter, which counts percentage occurrence of words which user entered. Counter has a mechanism to avoid duplicates. 
---
+++ src/test/java/com/schibsted/output/CounterTest.java
@@ -0,0 +1,63 @@
+package com.schibsted.output;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+
+    private Counter counter;
+    Map<String, Set<String>> wordsDepot;
+
+    @Test
+    public void shouldNotDisplayAnything() throws Exception {
+        //given
+        String expected = "";
+        String fileName = "cherry.txt";
+        Set<String> file = new HashSet<>();
+        file.add(fileName);
+        wordsDepot = new HashMap<>();
+        wordsDepot.put("CHERRY", file);
+        ByteArrayOutputStream actual = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actual));
+        counter = new Counter(wordsDepot);
+
+        //when
+        counter.printResult("some");
+
+
+        //then
+        assertThat(actual.toString(), is(equalTo(expected)));
+    }
+
+    @Test
+    public void shouldReturnSomePercentage() throws Exception {
+        //given
+        String expected = "cherry.txt : 66.66667%\n";
+        String fileName = "cherry.txt";
+        Set<String> file = new HashSet<>();
+        file.add(fileName);
+        wordsDepot = new HashMap<>();
+        wordsDepot.put("CHERRY", file);
+        ByteArrayOutputStream actual = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(actual));
+        counter = new Counter(wordsDepot);
+
+        //when
+        counter.printResult("cherry cherry lady");
+
+
+        //then
+        assertThat(actual.toString(), is(equalTo(expected)));
+    }
+
+}
